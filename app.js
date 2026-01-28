72d029e662dc445724c2dfc3d8d87b13076c55cc
isaiahotico <127643977+isaiahotico@users.noreply.github.com>
2026-01-28 08:29:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ const app = {
     },
 
     requestWithdraw: async () => {
-        if (currentUser.balance < 0.02) return alert("Minimum withdrawal is ₱1");
+        if (currentUser.balance < 1) return alert("Minimum withdrawal is ₱1");
         
         const req = {
             uid: userId,
